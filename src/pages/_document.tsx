9b9859a51a84d933f0604dc74ab578f1ee5b2333
Vincent Williams <vincentgwilliams@gmail.com>
2023-07-31 10:12:17 +0200

    update favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,6 +22,9 @@ class CustomDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
           />
           <link rel='apple-touch-icon' sizes='180x180' href='/images/apple-touch-icon.png' />
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
           <link rel='shortcut icon' href='/images/favicon.png' />
         </Head>
         <body>
